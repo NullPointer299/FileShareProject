0e826e3f3cbe3daf30ac3fb74eee9cd0962c5b3c
hirabo <basket.h223@gmail.com>
2019-01-25 19:40:00 +0900

    01251939

--- js/sort.js
+++ js/sort.js
@@ -131,11 +131,11 @@ function createMainFolder() {
     var temp = "";
     if (nowPage != "trash") {
         for (var v of showFolders) {
-            temp += "<div class=\"node\" ondblclick=jump(\'Main?req=cd&&name=" + v.myName + "&path=" + v.myPath + "\',\"post\") onclick=\"check(\'" + v.myName + "\')\" oncontextmenu=\"return rightclick(new Folder(\'" + v.myName + "\',1,\'" + v.myPath + "\',\'" + v.isPublic + "\'))\"><img class=\"folder\" src=\"../picture/folder.png\"><a href=\"#\"></a><div class=\"check_box\"></div><div class=\"check\" id=\"" + v.myName + "_check\"></div><input type=\"checkbox\" name=\'" + v.myName + "\' id=\'" + v.myName + "\'><div class=\"filename\">" + v.myName + "</div></div>";
+            temp += "<div class=\"node\" ondblclick=jump(\'Main?req=cd&name=" + v.myName + "&path=" + v.myPath + "\',\"post\") onclick=\"check(\'" + v.myName + "\')\" oncontextmenu=\"return rightclick(new Folder(\'" + v.myName + "\',1,\'" + v.myPath + "\',\'" + v.isPublic + "\'))\"><img class=\"folder\" src=\"../picture/folder.png\"><a href=\"#\"></a><div class=\"check_box\"></div><div class=\"check\" id=\"" + v.myName + "_check\"></div><input type=\"checkbox\" name=\'" + v.myName + "\' id=\'" + v.myName + "\'><div class=\"filename\">" + v.myName + "</div></div>";
         }
     } else {
         for (var v of showFolders) {
-            temp += "<div class=\"node\" ondblclick=jump(\'Trash?req=cd&&name=" + v.myName + "&path=" + v.myPath + "\',\"post\") onclick=\"check(\'" + v.myName + "\')\" oncontextmenu=\"return rightclick(new trashFolder(\'" + v.myName + "\',1,\'" + v.myPath + "\',\'" + v.isRestore + "\',\'" + v.myDispName +"\'))\"><img class=\"folder\" src=\"../picture/folder.png\"><a href=\"#\"></a><div class=\"check_box\"></div><div class=\"check\" id=\"" + v.myName + "_check\"></div><input type=\"checkbox\" name=\'" + v.myName + "\' id=\'" + v.myName + "\'><div class=\"filename\">" + v.myDispName + "</div></div>";
+            temp += "<div class=\"node\" ondblclick=jump(\'Trash?req=cd&name=" + v.myName + "&path=" + v.myPath + "\',\"post\") onclick=\"check(\'" + v.myName + "\')\" oncontextmenu=\"return rightclick(new trashFolder(\'" + v.myName + "\',1,\'" + v.myPath + "\',\'" + v.isRestore + "\',\'" + v.myDispName +"\'))\"><img class=\"folder\" src=\"../picture/folder.png\"><a href=\"#\"></a><div class=\"check_box\"></div><div class=\"check\" id=\"" + v.myName + "_check\"></div><input type=\"checkbox\" name=\'" + v.myName + "\' id=\'" + v.myName + "\'><div class=\"filename\">" + v.myDispName + "</div></div>";
         }
     }
     return temp;
